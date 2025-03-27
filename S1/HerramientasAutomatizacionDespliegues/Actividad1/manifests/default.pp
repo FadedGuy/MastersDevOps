31c1fa4ec779608ec1c99fc61bb552f11e2e2193
Kevin Aceves <kevin.aceves@hotmail.com>
2025-03-27 11:33:06 -0600

    HerramientasAutomatizacionDespliegues Actividad1 completada con templates

--- S1/HerramientasAutomatizacionDespliegues/Actividad1/manifests/default.pp
+++ S1/HerramientasAutomatizacionDespliegues/Actividad1/manifests/default.pp
@@ -1,10 +1,13 @@
+# Variables
 $document_root = '/vagrant'
 
+# Database
 $db_name = 'wordpress'
 $db_user = 'wordpress'
 $db_password = 'pa55w0rd!'
 $db_host = 'localhost'
 
+# WordPress 
 $auth_key = 'QJuoj#Ln7KY/^JA5MgNuDy+3xbR$RA+[}2V!|ISlVF>N]iv^0Gt$5U`9uk(3Pe8:'
 $secure_auth_key = 'k`2,dayLgx.,<jO?6sY;nW_WSRD~e<9-bs[?#.+&VGDWI,k47*;B|=:]~C-:;qFP'
 $logged_in_key = 'TED7j&B,|=DRxz&e,Ft|$4q~V9^_IN)B>|--;m(L:`h?x3[!oB|mo,|A{;jE(`m]'
@@ -14,20 +17,21 @@ $secure_auth_salt = '/lf`3+[=8`ZD$dw+01E<9$pZ+-w3m=pXp*qTTwE#!WfD~FTxc`F&.C~qZNz
 $logged_in_salt = 'n-h9.9J8Rx@3Sy4r-mO9+>XWxx(:_<}_ZN?oM^v-E:G,y+L!>a#+mcR0*=bFBr]-'
 $nonce_salt = '~AMBN|}+%8<8<!J*<q]GEJ~n*JD{+r3p3+9rEEL02GQ>Ne)TkB9V-BI>@S2-POl:'
 
+$wp_site_title = 'Generic Title'
+$wp_admin_user = 'admin'
+$wp_admin_password = 'admin'
+$wp_admin_email = 'admin@admin.com'
+$wp_url = 'http://localhost:8080'
+
 include apache
 include mysql
 include php
 include wordpress
 
-# exec { 'Skip Message':
-#   command => "echo ‘Este mensaje sólo se muestra si no se ha copiado el fichero index.html'",
-#   unless => "test -f ${document_root}/index.html",
-#   path => "/bin:/sbin:/usr/bin:/usr/sbin",
-# }
-
 exec { 'apt-update' :
   command => '/usr/bin/apt-get update',
 }
+# Actualizar los paquetes
 Exec['apt-update'] -> Package <| |>
 
 $ipv4_address = $facts['networking']['ip']
--- S1/HerramientasAutomatizacionDespliegues/Actividad1/modules/wordpress/manifests/init.pp
+++ S1/HerramientasAutomatizacionDespliegues/Actividad1/modules/wordpress/manifests/init.pp
@@ -1,10 +1,4 @@
 class wordpress {
-  $wp_site_title = 'Generic Title'
-  $wp_admin_user = 'admin'
-  $wp_admin_password = 'admin'
-  $wp_admin_email = 'admin@admin.com'
-  $wp_url = 'http://localhost:8080'
-
   # mkdir and chown
   # sudo mkdir -p /srv/www
   # sudo chown www-data: /srv/www
@@ -46,7 +40,6 @@ class wordpress {
   }
 
   exec { 'configure_wp_site' :
-    # command => 'wp core install  --title="My WordPress Site" --admin_user="admin" --admin_password="your_admin_password" --admin_email="you@example.com" --url="http://localhost:8080"',
     command => "wp core install --title='${wp_site_title}' --admin_user='${wp_admin_user}' --admin_password='${wp_admin_password}' --admin_email='${wp_admin_email}' --url='${wp_url}'",
     path    => ['/usr/local/bin', '/usr/bin'],
     cwd     => '/srv/www/wordpress',
