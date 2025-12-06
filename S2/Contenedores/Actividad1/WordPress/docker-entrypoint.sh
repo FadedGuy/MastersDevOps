bc43518b06de1670e11c573be1096f182f5915ed
Kevin Aceves <kevin.aceves@hotmail.com>
2025-12-06 16:53:09 -0600

    Update all

--- S2/Contenedores/Actividad1/WordPress/docker-entrypoint.sh
+++ S2/Contenedores/Actividad1/WordPress/docker-entrypoint.sh
@@ -1,37 +1,44 @@
 #!/bin/bash
 set -e
 
-# Asignar valores por defecto si no existen
-# Base de datos
+WP_PATH="/var/www/wordpress"
+
+# Si el directorio está vacío, copiar WordPress inicial
+if [ ! -f "$WP_PATH/wp-config-sample.php" ]; then
+    echo "WordPress directory empty. Copying initial WordPress files..."
+    cp -R /usr/src/wordpress/* "$WP_PATH"
+fi
+
+# Variables de entorno por defecto
 DB_NAME=${DB_NAME:-wordpress}
 DB_USER=${DB_USER:-wpuser}
 DB_PASSWORD=${DB_PASSWORD:-wppassword}
 DB_HOST=${DB_HOST:-wp-db}
 
-# Configuracion de WordPress
 WP_URL=${WP_URL:-http://localhost:8080}
 WP_TITLE=${WP_TITLE:-"Mi WordPress"}
 WP_ADMIN_USER=${WP_ADMIN_USER:-admin}
 WP_ADMIN_PASSWORD=${WP_ADMIN_PASSWORD:-adminpassword}
-WP_ADMIN_EMAIL=${WP_ADMIN_EMAIL:-kevin.aceves@hotmail.com}
+WP_ADMIN_EMAIL=${WP_ADMIN_EMAIL:-admin@example.com}
 
-# Copiar wp-config.php si no existe
-if [ ! -f /var/www/wordpress/wp-config.php ]; then
-    cp /var/www/wordpress/wp-config-sample.php /var/www/wordpress/wp-config.php
-    sed -i "s/database_name_here/$DB_NAME/" /var/www/wordpress/wp-config.php
-    sed -i "s/username_here/$DB_USER/" /var/www/wordpress/wp-config.php
-    sed -i "s/password_here/$DB_PASSWORD/" /var/www/wordpress/wp-config.php
-    sed -i "s/localhost/$DB_HOST/" /var/www/wordpress/wp-config.php
+# Crear wp-config.php si no existe
+if [ ! -f "$WP_PATH/wp-config.php" ]; then
+    cp "$WP_PATH/wp-config-sample.php" "$WP_PATH/wp-config.php"
 
-    # Instalar WordPress usando WP-CLI
+    sed -i "s/database_name_here/$DB_NAME/" "$WP_PATH/wp-config.php"
+    sed -i "s/username_here/$DB_USER/" "$WP_PATH/wp-config.php"
+    sed -i "s/password_here/$DB_PASSWORD/" "$WP_PATH/wp-config.php"
+    sed -i "s/localhost/$DB_HOST/" "$WP_PATH/wp-config.php"
+
+    # Instalar WordPress
     wp core install \
         --url="$WP_URL" \
         --title="$WP_TITLE" \
         --admin_user="$WP_ADMIN_USER" \
         --admin_password="$WP_ADMIN_PASSWORD" \
         --admin_email="$WP_ADMIN_EMAIL" \
-        --path="/var/www/wordpress"
+        --path="$WP_PATH"
 fi
 
-# Arrancar PHP-FPM
-php-fpm
+# Iniciar PHP-FPM
+exec php-fpm
--- S2/Contenedores/Actividad3/configuration/bastionUserData.sh
+++ S2/Contenedores/Actividad3/configuration/bastionUserData.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
+# Install git 
+yum update -y 
+yum install -y git
+
 # Install kubectl
-curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.28.3/2023-11-14/bin/linux/amd64/kubectl
+curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.33.3/2025-08-03/bin/linux/amd64/kubectl
 chmod +x ./kubectl
 mkdir -p /home/ec2-user/bin
 cp ./kubectl /home/ec2-user/bin/kubectl
 echo 'export PATH=$PATH:/home/ec2-user/bin' >> /home/ec2-user/.bashrc
+
+# # Install eksctl
+# ARCH=amd64
+# PLATFORM=$(uname -s)_$ARCH
+# curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
+# tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
+# sudo mv /tmp/eksctl /usr/local/bin
 
 # Create and add ~/.aws/credentials
 mkdir -p /home/ec2-user/.aws
@@ -21,3 +32,8 @@ chmod 600 /home/ec2-user/.aws/credentials
 
 # Create kubeconfig from cluster
 aws eks update-kubeconfig --name <CLUSTER_NAME> --region us-east-1
+
+# Copy repository with manifests
+cd /home/ec2-user
+git clone https://github.com/FadedGuy/MastersDevOps.git
+chwon -R ec2-user:ec2-user /home/ec2-user/MasterDevOps
